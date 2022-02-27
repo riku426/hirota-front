45c98e38a26efa94540a4d1221f7dc6ff40f1061
岩本陸 <>
2022-02-27 16:46:37 +0900

    no message

---
+++ components/Top3.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+}
+</style>
